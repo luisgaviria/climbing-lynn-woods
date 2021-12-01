45c5e5803dcc055c9fe83ebfe73d8c8dac439084
Luis Gaviria <luisgaviria@macbook-pro-2.mynetworksettings.com>
2021-12-01 16:36:08 -0500

    new edits styling

--- client/lynnwoods/src/containers/Rocks.js
+++ client/lynnwoods/src/containers/Rocks.js
@@ -27,7 +27,6 @@ const Rocks = () => {
 
   return (
     <>
-<<<<<<< HEAD
       <div className='rocks-grid'>
         {Object.keys(state.boulders)?.map((key) => {
           return (
@@ -35,15 +34,6 @@ const Rocks = () => {
               {" "}
               <div className='area'>
                 <h3 className='area-name'>{key}</h3>
-=======
-      <h1 style={{ textAlign: "center" }}>Lynn Woods Climbing:</h1>
-      <div className="rocks-grid">
-        {Object.keys(state.boulders)?.map((key) => {
-          return (
-            <>
-              <div className="area">
-                <h3>{key}</h3>
->>>>>>> bf6551f8776b5e325d28a90630004618a9b8dc7e
                 <Area area={state.boulders[key]} />
               </div>
             </>
